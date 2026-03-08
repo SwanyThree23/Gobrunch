195f235a93ccea790aa593b3303ef5ff8a56979a
SwanyThree23 <ljswanson72@gmail.com>
2026-03-08 12:21:08 -0700

    approve

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   output: 'standalone',
   images: {
-    domains: ['localhost'],
+    domains: ['localhost']
   },
+  experimental: {
+    serverActions: true
+  }
 };
 
 module.exports = nextConfig;
